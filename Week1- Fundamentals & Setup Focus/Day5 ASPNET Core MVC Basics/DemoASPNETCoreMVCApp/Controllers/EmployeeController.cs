4199419322ad8e10d7c031552796e9e85a91314f
CodeAdmirer <codeadmirer@gmail.com>
2025-04-12 23:59:30 +0530

    Added the basic MVC code setup

---
+++ Week1- Fundamentals & Setup Focus/Day5 ASPNET Core MVC Basics/DemoASPNETCoreMVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using DemoASPNETCoreMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoASPNETCoreMVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var emps = new List<Employee>
+        {
+            new Employee { Id = 1, Name = "Suri", Salary = 120000 },
+            new Employee { Id = 2, Name = "Mouni", Salary = 100000 }
+        };
+            return View(emps); // Pass data to View
+        }
+    }
+}
---
+++ Week1- Fundamentals & Setup Focus/Day5 ASPNET Core MVC Basics/DemoASPNETCoreMVCApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoASPNETCoreMVCApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
