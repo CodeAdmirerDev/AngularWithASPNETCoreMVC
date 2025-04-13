30cf686f4404c6e20627066d136d18799255f2f1
CodeAdmirer <codeadmirer@gmail.com>
2025-04-13 15:11:04 +0530

    Added routing related code

---
+++ Week1- Fundamentals & Setup Focus/Day6 ASPNET Core Routing/ASPNETCoreMVCWIthRouting/Controllers/EmployeeController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNETCoreMVCWIthRouting.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreMVCWIthRouting.Controllers
+{
+    [Route("employee")]
+    public class EmployeeController : Controller
+    {
+        private static List<Employee> _data = new List<Employee>
+    {
+        new Employee { Id = 1, Name = "Suri", Salary = 120000 },
+        new Employee { Id = 2, Name = "Mouni", Salary = 110000 }
+    };
+
+        [HttpGet]
+        [Route("index")]
+        public IActionResult Index() => View(_data);
+
+        [HttpGet]
+        [Route("details/{id}")]
+        public IActionResult Details(int id)
+        {
+            var emp = _data.FirstOrDefault(e => e.Id == id);
+            if (emp == null) return NotFound();
+            return View(emp);
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        [Route("create")]
+        public IActionResult Create(Employee emp)
+        {
+            emp.Id = _data.Max(e => e.Id) + 1;
+            _data.Add(emp);
+            return RedirectToAction("Index");
+        }
+    }
+}
