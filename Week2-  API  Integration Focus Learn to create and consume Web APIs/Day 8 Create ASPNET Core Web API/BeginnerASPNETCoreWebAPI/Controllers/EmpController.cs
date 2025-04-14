2db2a8fd461150a6662e12fe11fe50c728696de3
CodeAdmirer <codeadmirer@gmail.com>
2025-04-14 21:15:46 +0530

    📚 Key Concepts: Create Web API Project [ApiController] usage Routing: [Route], [HttpGet/Post] Return simple JSON Accept JSON body using [FromBody] Use DTOs with [Required], [Range] validation

---
+++ Week2-  API  Integration Focus Learn to create and consume Web APIs/Day 8 Create ASPNET Core Web API/BeginnerASPNETCoreWebAPI/Controllers/EmpController.cs
@@ -0,0 +1,55 @@
+﻿using BeginnerASPNETCoreWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeginnerASPNETCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        List<Emp> emps;
+
+        public EmpController()
+        {
+            emps = new List<Emp>()
+            {
+
+                new Emp() {Address = "Address1", Email = "Email1", Id = 1, Name = "CodeAdmirer", salary=12000},
+                new Emp() {Address = "Address2", Email = "Email2", Id = 2, Name = "Name2", salary=150000},
+            };
+        }
+
+
+        [HttpGet]
+        public JsonResult GetAllEmps()
+        {
+            //retrun json result
+
+            return new JsonResult(emps); // 👈 Explicitly returns JSON
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetEmpById(int id)
+        {
+            var emp = emps.FirstOrDefault(e => e.Id == id);
+            if (emp == null)
+            {
+                return NotFound();
+            }
+            return Ok(emp);
+        }
+
+        [HttpPost]
+        public IActionResult PostEmp([FromBody]Emp emp) {
+            if (emp == null)
+            {
+                return BadRequest();
+            }
+            emps.Add(emp);
+            return CreatedAtAction(nameof(GetEmpById), new { id = emp.Id }, emp);
+        }
+
+    }
+
+}
---
+++ Week2-  API  Integration Focus Learn to create and consume Web APIs/Day 8 Create ASPNET Core Web API/BeginnerASPNETCoreWebAPI/Program.cs
@@ -0,0 +1,34 @@
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace BeginnerASPNETCoreWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+                options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+            }); 
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
