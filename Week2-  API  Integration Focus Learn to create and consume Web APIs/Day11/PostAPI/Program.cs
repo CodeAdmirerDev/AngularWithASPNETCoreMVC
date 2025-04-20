6359670c764cd61297f32e04f442e9e29b9f43e9
CodeAdmirer <codeadmirer@gmail.com>
2025-04-20 23:52:22 +0530

    Added CROS related

--- Week2-  API  Integration Focus Learn to create and consume Web APIs/Day11/PostAPI/Program.cs
+++ Week2-  API  Integration Focus Learn to create and consume Web APIs/Day11/PostAPI/Program.cs
@@ -15,6 +15,19 @@ namespace PostAPI
             builder.Services.AddSingleton<PostRepositrory>();
             builder.Services.AddSwaggerGen();
 
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAngularApp",
+                    policy =>
+                    {
+                        policy.WithOrigins("http://localhost:4200")
+                              .AllowAnyHeader()
+                              .AllowAnyMethod();
+                    });
+            });
+
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
@@ -28,7 +41,7 @@ namespace PostAPI
 
             app.UseAuthorization();
 
-
+            app.UseCors("AllowAngularApp");
             app.MapControllers();
 
             app.Run();
