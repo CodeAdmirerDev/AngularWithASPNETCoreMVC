f9dd9241c86bee5486d175a737b2de9f5e1943a5
CodeAdmirer <codeadmirer@gmail.com>
2025-05-10 14:04:19 -0700

    Usage example for Observable , Subscription

--- Week3/Day15.1/counterApp-Demo-using-ngrx/src/app/counter/counter-output/counter-output.component.ts
+++ Week3/Day15.1/counterApp-Demo-using-ngrx/src/app/counter/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CounterState } from '../store/counter.state';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
@@ -8,18 +9,31 @@ import { CounterState } from '../store/counter.state';
   templateUrl: './counter-output.component.html',
   styleUrl: './counter-output.component.css'
 })
-export class CounterOutputComponent implements OnInit {
+export class CounterOutputComponent implements OnInit  { //OnDestroy
 
+  counterSubscription!: Subscription;
 
   counter!: number;
+
+  counter$! :Observable<{counter:number}>;
   constructor(private store: Store<{ counter: CounterState }>) { }
 
   ngOnInit(): void {
-    this.store.select('counter').subscribe((data: CounterState) => {
+
+
+   this.counterSubscription = this.store.select('counter').subscribe((data: CounterState) => {
       this.counter = data.counter;
     });
 
+    this.counter$ = this.store.select('counter');
+
   }
+
+  // ngOnDestroy() {
+  //   if(this.counterSubscription){
+  //     this.counterSubscription.unsubscribe();
+  //   }
+  // }
 
 
 }
